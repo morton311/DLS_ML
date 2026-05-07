3ed136bd140968c0a550fbb0ec12cf54cf21ebd2
morton311 <morton.311@osu.edu>
2026-05-07 10:43:48 -0400

    new case

--- challenge_job.sh
+++ challenge_job.sh
@@ -49,7 +49,7 @@ source $HOME/.venv/bin/activate
 # python3 main.py -c 'challenge/dls/p11m10/deep_swiglu' -m 'eval'
 # python3 main.py -c 'challenge/dls/p11m10/deep_swiglu' -m 'latent' 
 
-python3 main.py -c 'challenge/dls/p15m10/swiglu2' -m 'train'
+python3 main.py -c 'challenge/dls/p15m10/swiglu2' -m 'train' -d 'True'
 python3 main.py -c 'challenge/dls/p15m10/swiglu2' -m 'pred'
 python3 main.py -c 'challenge/dls/p15m10/swiglu2' -m 'eval'
 python3 main.py -c 'challenge/dls/p15m10/swiglu2' -m 'latent'
