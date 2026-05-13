498b4941fd65a207a846cc00fd351e07c3396d26
morton311 <morton.311@osu.edu>
2026-05-13 16:28:13 -0400

    fix challenge job

--- challenge_job.sh
+++ challenge_job.sh
@@ -10,49 +10,49 @@
 #SBATCH -t 04:00:00
 #SBATCH --output=out_challenge.out
 
-# module use $HOME/my_modules
-# module load torch_module
-# source $HOME/.venv/bin/activate
+module use $HOME/my_modules
+module load torch_module
+source $HOME/.venv/bin/activate
 
 # python3 main.py -c 'challenge/dls/p25m10/swiglu' -m 'train'
 # python3 main.py -c 'challenge/dls/p25m10/swiglu' -m 'pred'
 # python3 main.py -c 'challenge/dls/p25m10/swiglu' -m 'eval'
-python3 main.py -c 'challenge/dls/p25m10/swiglu' -m 'latent'
+# python3 main.py -c 'challenge/dls/p25m10/swiglu' -m 'latent'
 
 # python3 main.py -c 'challenge/dls/p25m10/small' -m 'train'
 # python3 main.py -c 'challenge/dls/p25m10/small' -m 'pred'
 # python3 main.py -c 'challenge/dls/p25m10/small' -m 'eval'
-python3 main.py -c 'challenge/dls/p25m10/small' -m 'latent'
+# python3 main.py -c 'challenge/dls/p25m10/small' -m 'latent'
 
 # python3 main.py -c 'challenge/dls/p25m20/small' -m 'train'
 # python3 main.py -c 'challenge/dls/p25m20/small' -m 'pred'
 # python3 main.py -c 'challenge/dls/p25m20/small' -m 'eval'
-python3 main.py -c 'challenge/dls/p25m20/small' -m 'latent'
+# python3 main.py -c 'challenge/dls/p25m20/small' -m 'latent'
 
 # python3 main.py -c 'challenge/dls/p25m10/underfit' -m 'train'
 # python3 main.py -c 'challenge/dls/p25m10/underfit' -m 'pred'
 # python3 main.py -c 'challenge/dls/p25m10/underfit' -m 'eval'
-python3 main.py -c 'challenge/dls/p25m10/underfit' -m 'latent'
+# python3 main.py -c 'challenge/dls/p25m10/underfit' -m 'latent'
 
 # python3 main.py -c 'challenge/dls/p25m20/underfit' -m 'train'
 # python3 main.py -c 'challenge/dls/p25m20/underfit' -m 'pred'
 # python3 main.py -c 'challenge/dls/p25m20/underfit' -m 'eval'
-python3 main.py -c 'challenge/dls/p25m20/underfit' -m 'latent'
+# python3 main.py -c 'challenge/dls/p25m20/underfit' -m 'latent'
 
 # python3 main.py -c 'challenge/dls/p11m10/case' -m 'train'
 # python3 main.py -c 'challenge/dls/p11m10/case' -m 'pred'
 # python3 main.py -c 'challenge/dls/p11m10/case' -m 'eval'
-python3 main.py -c 'challenge/dls/p11m10/case' -m 'latent'
+# python3 main.py -c 'challenge/dls/p11m10/case' -m 'latent'
 
 # python3 main.py -c 'challenge/dls/p11m10/deep_swiglu' -m 'train'
 # python3 main.py -c 'challenge/dls/p11m10/deep_swiglu' -m 'pred'
 # python3 main.py -c 'challenge/dls/p11m10/deep_swiglu' -m 'eval'
-python3 main.py -c 'challenge/dls/p11m10/deep_swiglu' -m 'latent' 
+# python3 main.py -c 'challenge/dls/p11m10/deep_swiglu' -m 'latent' 
 
 # torchrun main.py -c 'challenge/dls/p15m10/swiglu2' -m 'train' -d 'True'
 # python3 main.py -c 'challenge/dls/p15m10/swiglu2' -m 'pred'
 # python3 main.py -c 'challenge/dls/p15m10/swiglu2' -m 'eval'
-python3 main.py -c 'challenge/dls/p15m10/swiglu2' -m 'latent'
+# python3 main.py -c 'challenge/dls/p15m10/swiglu2' -m 'latent'
 
 # torchrun main.py -c 'challenge/pod/tr_50m' -m 'train' -d 'True'
 # python3 main.py -c 'challenge/pod/tr_50m' -m 'pred'
